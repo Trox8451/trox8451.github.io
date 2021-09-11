132e1d4da8415607fa772fa936cb05377bdbcca5
Jacksyn T <78824880+JTrox505@users.noreply.github.com>
2021-09-11 11:33:10 -0600

    Created base for js

---
+++ main.js
@@ -0,0 +1,23 @@
+var random_number = Math.random();
+random_number *= 10; random_number+=1;
+random_number = Math.floor(random_number);
+var number_of_guesses = 0
+
+function guess_the_number(number) {
+	while(true) {
+		number_guessed = prompt(`guess a number between 1 and 10, the number is ${number}`);
+		number_of_guesses++;
+		if(number==number_guessed) {
+			alert(`Wow you actually guessed the right one, it took you ${number_of_guesses} tries`);
+			break;
+		}
+		else if(number>number_guessed) {
+			alert(`No you're stupid and I hate you, your number is too low`);
+		}
+		else if(number<number_guessed) {
+			alert(`No you're stupid and I hate you, your number is too high`);
+		}
+	}
+}
+
+guess_the_number(random_number);
