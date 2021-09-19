7d8f7877de558135803ac639f18a68727b26668a
Jacksyn T <78824880+JTrox505@users.noreply.github.com>
2021-09-19 12:19:48 -0600

    changed guess_number function
    changed it so that the prompt didn't tell the user what the number was

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var number_of_guesses = 0
 
 function guess_the_number(number=random_number) {
 	while(true) {
-		number_guessed = prompt(`guess a number between 1 and 10, the number is ${number}`);
+		number_guessed = prompt(`guess a number between 1 and 10`);
 		number_of_guesses++;
 		if(number==number_guessed) {
 			alert(`Wow you actually guessed the right one, it took you ${number_of_guesses} tries`);
