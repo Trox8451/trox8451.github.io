5aa0a3b099bacf0a817b7de3b37d96ef3cb36d6a
Jacksyn T <78824880+JTrox505@users.noreply.github.com>
2021-09-19 12:33:31 -0600

    fixed bug
    The random number wasn't changing every time you clicked the button since the page only loaded once so now the number is generated every time the function is called, also the number_of_tries will reset now.

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-var random_number = Math.random();
-random_number *= 10; random_number++;
-random_number = Math.floor(random_number);
-var number_of_guesses = 0
 
-function guess_the_number(number=random_number) {
+function guess_the_number() {
+	var random_number = Math.random();
+	random_number *= 10; random_number++;
+	random_number = Math.floor(random_number);
+	var number_of_guesses = 0
 	while(true) {
 		number_guessed = prompt(`guess a number between 1 and 10`);
 		number_of_guesses++;
-		if(number==number_guessed) {
+		if(random_number==number_guessed) {
 			alert(`Wow you actually guessed the right one, it took you ${number_of_guesses} tries`);
 			break;
 		}
-		else if(number>number_guessed) {
+		else if(random_number>number_guessed) {
 			alert(`No you're stupid and I hate you, your number is too low`);
 		}
-		else if(number<number_guessed) {
+		else if(random_number<number_guessed) {
 			alert(`No you're stupid and I hate you, your number is too high`);
 		}
 	}
